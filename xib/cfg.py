c79e7ceb5e8b9e61449cca08e0feac5b06b9020d
j-luo93 <j_luo@mit.edu>
2019-10-13 00:23:06 -0400

    all langs

--- xib/cfg.py
+++ xib/cfg.py
@@ -5,7 +5,8 @@ reg = Registry('cfg')
 
 @reg
 class TestEn:
-    data_path: str = 'data/phones_en_idx.pth'
+    lang = 'en'
+
     dim: int = 20
     num_features: int = 112
     num_feature_groups: int = 14
@@ -14,6 +15,9 @@ class TestEn:
     num_steps: int = 10000
     window_size: int = 3
     hidden_size: int = 100
+
+    def __post_init__(self):
+        self.data_path = f'data/phones_{self.lang}_idx.pth'
 
 
 @reg
@@ -29,3 +33,16 @@ class TestEnPCV(TestEn):
 @reg
 class TestEnPDST(TestEn):
     mode: str = 'pdst'
+
+
+_all_langs = [
+    'sh', 'bg', 'fr', 'de', 'lt', 'pt', 'nl', 'ka', 'is', 'ro', 'fi', 'it', 'eo',
+    'el', 'cs', 'syc', 'ga', 'ang', 'hy', 'cy', 'tr', 'ms', 'ady', 'sk', 'da', 'fa',
+    'gem-pro', 'sl', 'lb', 'es', 'nci', 'gl', 'fo', 'enm', 'io', 'dsb', 'ba', 'tlh',
+    'sv', 'no', 'tl', 'la', 'jbo', 'arc', 'he', 'sq', 'ps', 'nn', 'az', 'sga', 'sco', 'yue'
+]
+
+for lang in _all_langs:
+    cap_lang = lang[0].upper() + lang[1:]
+    new_cls = type(f'Test{cap_lang}PCV', (TestEnPCV,), {'lang': lang})
+    reg(new_cls)

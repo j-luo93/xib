fixed the mlm weighting bug

xib/model/decipher_model.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
-from xib.model.modules import Predictor
 from dataclasses import InitVar, dataclass
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -22,7 +21,8 @@ from xib.check_in_vocab_impl import \
 from xib.data_loader import ContinuousTextIpaBatch, IpaBatch
 from xib.extract_words_impl import extract_words_v8 as extract_words  # pylint: disable=no-name-in-module
 from xib.ipa import Category, should_include
-from xib.ipa.process import Segmentation, Span, O
+from xib.ipa.process import O, Segmentation, Span
+from xib.model.modules import Predictor
 
 from . import BT, FT, LT
 from .lm_model import LM
@@ -106,7 +106,7 @@ class PositionalEmbedding(nn.Module):
     def forward(self, positions: LT):
         with NoName(self.embeddings, positions):
             ret = self.embeddings[positions]
-        new_names = positions.names + ('char_emb_for_label', )
+        new_names = positions.names + ('char_emb', )
         return ret.refine_names(*new_names)
 
 
@@ -164,7 +164,7 @@ class DecipherModel(nn.Module):
         freeze(self.lm_model)
 
         # NOTE(j_luo) I'm keeping a separate embedding for label prediction.
-        self.emb_for_label = FeatEmbedding('feat_emb_for_label', 'chosen_feat_group', 'char_emb_for_label')
+        self.emb_for_label = FeatEmbedding('feat_emb_for_label', 'chosen_feat_group', 'char_emb')
 
         cat_dim = g.dim * self.emb_for_label.effective_num_feature_groups
         self.self_attn_layers = nn.ModuleList()
@@ -204,18 +204,23 @@ class DecipherModel(nn.Module):
         else:
             raise NotImplementedError()
 
-    def forward(self, batch: ContinuousTextIpaBatch, mode: str):
+    def forward(self, batch: Union[ContinuousTextIpaBatch, IpaBatch], mode: str):
         bs = batch.batch_size
         ret = dict()
 
         # ------------------------- Local mode ------------------------- #
 
         # Get the samples of label sequences first.
-        out = self.emb_for_label(batch.feat_matrix, batch.source_padding)
+        # HACK(j_luo)
+        masked_positions = None
+        if isinstance(batch, IpaBatch):
+            masked_positions = batch.pos_to_predict
+        out = self.emb_for_label(batch.feat_matrix, batch.source_padding, masked_positions=masked_positions)
+
         positions = get_named_range(batch.feat_matrix.size('length'), name='length')
-        pos_emb = self.positional_embedding(positions)
+        pos_emb = self.positional_embedding(positions).align_as(out)
         out = out + pos_emb
-        out = out.align_to('length', 'batch', 'char_emb_for_label')
+        out = out.align_to('length', 'batch', 'char_emb')
         for i, layer in enumerate(self.self_attn_layers):
             # out, _ = self_attend(layer, out, f'self_attn_repr')
             with NoName(out, batch.source_padding):

xib/model/modules.py
@@ -59,7 +59,7 @@ class FeatEmbedding(nn.Module):
     def _get_embeddings(self):
         return nn.Embedding(g.num_features, g.dim)
 
-    def forward(self, feat_matrix: LT, padding: BT) -> FT:
+    def forward(self, feat_matrix: LT, padding: BT, masked_positions: Optional[LT] = None) -> FT:
         feat_matrix = adv_index(feat_matrix, 'feat_group', self.c_idx)
         # Convert old style to new style ipa features.
         if g.new_style:
@@ -79,6 +79,13 @@ class FeatEmbedding(nn.Module):
         feat_emb = feat_emb.align_to('batch', 'length', self.char_emb_name)
         padding = padding.align_to('batch', 'length')
         feat_emb.rename(None)[padding.rename(None)] = 0.0
+
+        if masked_positions is not None:
+            batch_i = get_range(padding.size('batch'), 1, 0)
+            feat_emb = feat_emb.align_to('batch', 'char_emb', 'length')
+            # feat_emb = self.feat_embeddings(feat_matrix).view(bs, l, -1).transpose(1, 2)  # size: bs x D x l
+            # TODO(j_luo) ugly
+            feat_emb.rename(None)[batch_i, :, masked_positions.rename(None)] = 0.0
         return feat_emb
 
 
@@ -127,12 +134,8 @@ class Encoder(nn.Module):
     def forward(self, feat_matrix: LT, pos_to_predict: LT, source_padding: BT) -> FT:
         bs = source_padding.size('batch')
         l = source_padding.size('length')
-        feat_emb = self.feat_embedding(feat_matrix, source_padding)
-        feat_emb = feat_emb.align_to('batch', 'char_emb', 'length')
-        # feat_emb = self.feat_embeddings(feat_matrix).view(bs, l, -1).transpose(1, 2)  # size: bs x D x l
         batch_i = get_range(bs, 1, 0)
-        # TODO(j_luo) ugly
-        feat_emb.rename(None)[batch_i, :, pos_to_predict.rename(None)] = 0.0
+        feat_emb = self.feat_embedding(feat_matrix, source_padding, masked_positions=pos_to_predict)
         output = self.conv_layers(feat_emb.rename(None))
         output = output.refine_names('batch', 'char_conv_repr', 'length')  # size: bs x D x l
         output = self.linear(output.align_to(..., 'char_conv_repr'))  # size: bs x l x n_hid
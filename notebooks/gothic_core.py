e19e9982ffd1b045f5b28b171ced0bc476f931c4
j-luo93 <j_luo@mit.edu>
2019-12-21 23:36:18 -0500

    extract tsv from html files

--- notebooks/gothic_core.py
+++ notebooks/gothic_core.py
@@ -10,6 +10,7 @@ from typing import (Container, Dict, Iterable, List, NewType, Optional,
                     Sequence, Tuple, TypeVar, Union)
 
 import pandas as pd
+from lxml.html import HTMLParser, fromstring
 from pyquery import PyQuery as pq
 
 from dev_misc.utils import Singleton, concat_lists
@@ -673,3 +674,107 @@ class EtymologicalDictionary:
         lang2_seq = [lang2] * len(word2_seq)
         ety_dict = EtymologicalDictionary(lang1_seq, word1_seq, lang2_seq, word2_seq)
         return ety_dict
+
+
+# -------------------------------------------------------------- #
+#                  Convert html files into tsv.                  #
+# -------------------------------------------------------------- #
+
+reserved_seps = {
+    'got': ['ÜG', 'ÜE', 'Vw', 'Hw', 'Q', 'I', 'E', 'B', 'R', 'L', 'Son'],
+    'edel': ['Vw', 'Hw', 'Q', 'I', 'E', 'L', 'Son', 'GB', 'BM', 'F'],
+    'germ': ['RB', 'Vw', 'Hw', 'Q', 'I', 'E', 'W', 'B', 'L', 'Son'],
+    'idg': ['RB', 'ÜG', 'Vw', 'Hw', 'E', 'W', 'L', 'Son'],
+    'ae': ['ÜG', 'Vw', 'Hw', 'Q', 'I', 'E', 'W', 'R', 'L', 'Son']
+}
+
+
+def nm(dataset: str, sep: Optional[str] = None) -> str:  # Stands for negative match.
+    to_avoid = reserved_seps[dataset]
+    if sep is not None:
+        to_avoid = reserved_seps[dataset][reserved_seps[dataset].index(sep) + 1:]
+    reserved_p = ''.join([fr'(?!{sep}\.:)' for sep in to_avoid])
+    return reserved_p
+
+
+def get_regex(dataset: str, sep: Optional[str] = None) -> str:
+    reserved_p = nm(dataset, sep)
+    if sep is None:
+        prefix = ''
+    else:
+        prefix = f'{sep}\.:'
+    return fr'({prefix}(?:{reserved_p}.)*;?\s*)?'
+
+
+def get_match_regex(dataset: str) -> re.Pattern:
+    lemma_p = r'^([^,]+),?\s*'
+    alternative_p = r'((?:\b[^\.]+\b)(?!\.),?\s*)*'
+    lang_p = r'([^\.]+\.,?\s*)'
+    wortarten_p = r'([^:]+:\s*)?'
+    bedeutung_p = get_regex(dataset)
+
+    patterns = [lemma_p, alternative_p, lang_p, wortarten_p, bedeutung_p]
+    for sep in reserved_seps[dataset][:-1]:
+        patterns.append(get_regex(dataset, sep))
+    # The last pattern needs special treatment.
+    last_sep = reserved_seps[dataset][-1]
+    patterns.append(fr'({last_sep}\.:(?:{nm(dataset, last_sep)}.)*)?\s*$')
+    regex = re.compile(''.join(patterns))
+    return regex
+
+
+group2col = {
+    dataset: ['Lemma', 'Alternative', 'Sprachen', 'Wortarten_all', 'Bedeutungen_all'] + seps
+    for dataset, seps in reserved_seps.items()
+}
+
+
+def remove_sep(prefix: str, s: str) -> str:
+    """Remove separators."""
+    if s is not None:
+        return re.compile(fr'{prefix}\.:\s*([^;]*);?\s*').match(s).group(1)
+
+
+def get_df_from_doc(doc: Iterable[str], dataset: str) -> pd.DataFrame:
+    regex = get_match_regex(dataset)
+    matches = list()
+    for line in doc:
+        matches.append(regex.match(line.strip()))
+
+    # Extract columns from matches.
+    data = list()
+    for i, match in enumerate(matches):
+        row = [match.group(idx) for idx, col in enumerate(group2col[dataset], 1)]
+        data.append(tuple(row))
+
+    df = pd.DataFrame(data, columns=group2col[dataset])
+
+    for sep in reserved_seps[dataset]:
+        df[sep] = df[sep].apply(lambda s: remove_sep(sep, s))
+
+    return df
+
+
+def get_pq_doc(path: str, encoding: str):
+    """PyQuery has some issue dealing with broken html files. See https://github.com/gawel/pyquery/issues/31."""
+    parser = HTMLParser(encoding=encoding)
+
+    with open(path, encoding=encoding) as fin:
+        contents = fin.read()
+
+    doc = pq(fromstring(contents, parser=parser))
+    return doc
+
+
+class LineIterable:
+
+    def __init__(self, doc):
+        self.doc = doc
+
+    def __iter__(self):
+        for elem in chain(doc('p.MsoNormal'), doc('p.MsoPlainText')):
+            entry = pq(elem)
+            text = entry.text()
+            text = re.sub(r'\s', ' ', text).strip()  # EDEL has many multi-line contents.
+            if text:  # Some entries are empty.
+                yield text

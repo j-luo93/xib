91c24e52b4a2f99a86fd5e71606b9e671d67ad71
j-luo93 <j_luo@mit.edu>
2020-01-17 21:29:04 -0500

    basic protocol for generating a data file

---
+++ xib/data_prepare/table.py
@@ -0,0 +1,100 @@
+from typing import ClassVar, List, Set
+
+import pandas as pd
+
+PD_DF = pd.DataFrame
+
+
+class BaseTable:
+
+    _required_columns: ClassVar[List[str]] = None
+
+    def __init__(self, data: PD_DF):
+        self._check_columns(data)
+        self.data = data
+
+    def _check_columns(self, data: PD_DF):
+        cls = type(self)
+        for col in cls._required_columns:
+            if col not in data.columns:
+                raise TypeError(f'Column {col} not present in the data frame which has columns {data.columns}.')
+
+
+class MonoTable(BaseTable):
+
+    def __init__(self, data: PD_DF, lang: str):
+        super().__init__(data)
+        self.lang = lang
+
+
+class Tokens(MonoTable):
+    """`Tokens` class defines the sequential structure of a corpus."""
+
+    _required_columns = ['SegmentID', 'Position', 'Token', 'LemmaID']
+
+
+class Lemmas(MonoTable):
+
+    _required_columns = ['LemmaID', 'Lemma']
+
+
+class Stems(MonoTable):
+    """`Stems` class defines the mapping between tokens and stems."""
+
+    _required_columns = ['Token', 'Stems']
+
+
+class CogSet(BaseTable):
+
+    _required_columns = ['CogID', 'Source', 'Lemma', 'Language']
+
+    def __init__(self, data: PD_DF, source_lang: str):
+        super().__init__(data)
+        self.source_lang = source_lang
+
+
+def generate_data_file(lost_tokens: Tokens,
+                       lost_lemmas: Lemmas,
+                       lost_stems: Stems,
+                       known_stems: Stems,
+                       cog_set: CogSet,
+                       out_path: str):
+    # Get some meta-data first.
+    source_lang = cog_set.source_lang
+    lost_lang = lost_tokens.lang
+    known_lang = known_stems.lang
+    assert lost_lemmas.lang == lost_stems.lang == lost_lang
+
+    # Get stem info for each token in the corpus.
+    ts = pd.merge(lost_tokens.data, lost_stems.data, left_on='Token', right_on='Token', how='left')
+    # Add lemma information.
+    tsl = pd.merge(ts, lost_lemmas.data, left_on='LemmaID', right_on='LemmaID', how='left')
+
+    # Get cognate alignments.
+    def get_lemmas(lang: str, group) -> Set[str]:
+        if lang == source_lang:
+            lemmas = group.Source.values
+        lemmas = group[group.Language == lang].Lemma.values
+        return set(lemmas)
+
+    data = list()
+    for cog_id, group in cog_set.data.groupby('CogID'):
+        langs = set(group['Language'])
+        langs.add(source_lang)
+        if lost_lang in langs and known_lang in langs:
+            lost_lemmas = get_lemmas(lost_lang, group)
+            known_lemmas = get_lemmas(known_lang, group)
+            data.append((lost_lemmas, known_lemmas))
+    cog_df = pd.DataFrame(data, columns=['lost_lemma', 'known_lemma'])
+    cog_df = cog_df.explode('lost_lemma')
+
+    # Add stem info on the known side.
+    flat_cog_df = cog_df.explode('known_lemma')
+    flat_cog_df = pd.merge(flat_cog_df, known_stems, left_on='known_lemma', right_on='Token', how='left')
+    cog_df = flat_cog_df.pivot_table(index='lost_lemma', values='known_lemma', aggfunc=lambda lst: '|'.join(lst))
+
+    # Match lost tokens and known cognates based on lemmas.
+    matched = pd.merge(tsl, cog_df, left_on='Lemma', right_on='lost_lemma', how='left')
+
+    # Write to file.
+    matched.to_csv(out_path, sep='\t')
